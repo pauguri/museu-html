ae2f9dab3887f95f2deb349e59f266fb2857eda7
Pau <pauguvi@gmail.com>
2024-02-20 16:29:24 +0100

    remove practiques frontend

--- practiques/parcial2/js/main.js
+++
@@ -1,7 +0,0 @@
-const menu = document.getElementById('menu');
-const mList = document.getElementById('menu-list');
-
-function toggleMenu(){
-    menu.classList.toggle('open');
-    mList.classList.toggle('expanded');
-}
