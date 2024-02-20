0f2ba9489eef11981bcfd26d0f69c0167d4cd6f4
Pau <pauguvi@gmail.com>
2024-02-20 16:36:49 +0100

    delete backend projects (moved to galeria-backend)

--- practiques_backend/P03/backup/footer.php
+++
@@ -1,4 +0,0 @@
-</main>
-<footer>
-    Pau Guri Viura (c) 2022
-</footer>
--- practiques_backend/P03/footer.php
+++
@@ -1,8 +0,0 @@
-<!-- Pau Guri Viura - Practica 3 -->
-
-<!-- El footer se incluirá al final de todas las páginas -->
-
-</main>
-<footer>
-    Pau Guri Viura (c) 2022
-</footer>
